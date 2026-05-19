f03ef5528adc3a878541ac62d8c0a3b8d2f0173e
x1a0qiya <mn124356@163.com>
2026-05-19 15:00:26 +0800

    /

--- backend/app/main.py
+++ backend/app/main.py
@@ -200,7 +200,7 @@ def analyze_code(payload: CodeAnalysisRequest):
 CONTAINER_NAME = "global-judger"
 
 @app.post("/api/judge")
-def judge(req: JudgeRequest):
+def run_sjudge(req: JudgeRequest):
     run_id = str(uuid.uuid4())
     work_dir = f"/workspace/{run_id}"
 
